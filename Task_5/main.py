07b04d85219633c7a90b45017fd0d301c156fa3a
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-12-15 20:08:49 +0100

    implemented the update feature for the contact book

--- Task_5/main.py
+++ Task_5/main.py
@@ -2,6 +2,8 @@ from art import logo, options, added
 
 contact_list_store = []
 open_List = True
+change_details_choice = [1,2,3,4]
+
 
 def AddContactToList(name, phone_no, email, address):
     contact_list_store.append(
@@ -21,12 +23,46 @@ def ViewContact():
         output = []
 
         for index, entry in enumerate(contact_list_store, 1):
-            output.append(f"{index}. {entry["name"]} - {entry["phone"]}")
+            output.append(f"{index}. {entry["name"]} - {entry["phone"]} - {entry["email"]} - {entry["address"]}")
 
         result = "\n".join(output)
 
         print(result)
     
+def ChangeDetails(index_name, key):
+    global contact_list_store
+
+    contact_list_store[index_name][key] = input(f"Update your {key}: ")
+    print(added)
+
+
+def CheckChoice(search_name, choice):
+    # this commented lines is for when a user wants to update all details at once
+    # myDic = {
+    #     1:ChangeDetails(search_name, "name"),
+    #     2:ChangeDetails(search_name ,"phone"),
+    #     3:ChangeDetails(search_name, "email"),
+    #     4:ChangeDetails(search_name, "address"),
+    # }
+
+    # for x in myDic:
+    #     if choice == x:
+    #         return myDic[x]
+        
+    # return 0
+
+
+    if choice == 1:
+        ChangeDetails(search_name, "name")
+    elif choice == 2:
+        ChangeDetails(search_name, "phone")
+    elif choice == 3:
+        ChangeDetails(search_name, "email")
+    elif choice == 4:
+        ChangeDetails(search_name, "address")
+    else:
+        print("Invalid!!! Please enter the serial number for the particular details you want to update")
+
 
 def AddContact():
     name = input("Enter name: ")
@@ -35,13 +71,6 @@ def AddContact():
     address = input("Enter Address: ")
 
     AddContactToList(name, phone_no, email, address)
-
-# def SearchContact(name):
-#     for contact in contact_list_store:
-#         if contact["name"] == name:
-#             print(f"{contact["name"]} - {contact["phone"]}")
-#             return
-#     print("Contact Not Found!!!")
 
 def SearchContact(name):
     global contact_list_store
@@ -52,7 +81,15 @@ def SearchContact(name):
     print("Contact Not Found!!!")
     return -1
 
-# def UpdateContact()
+def UpdateContact(name):
+    search_name = SearchContact(name)
+    if search_name != -1:
+        choice = int(input("1. Update Name\n2. Update Phone Number\n3. Update Email\n4. Update Address... "))
+        res = CheckChoice(search_name, choice)
+        # if res == 0:
+        #     print("Invalid")
+
+
 def DeleteContact(name):
     global contact_list_store
     search_result = SearchContact(name)
@@ -73,6 +110,8 @@ def OpenContactList():
         ViewContact()
     elif choice == 3:
         res = SearchContact(input("which contact are you searching for?...").lower())
+    elif choice == 4:
+        res = UpdateContact(input("Type the name of the contact you want to update?... ").lower())
     elif choice == 5:
         DeleteContact(input("What is the name of the contact you wish to delete?...").lower())
     elif choice == 6:
